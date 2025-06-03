65cc8de02b3fd177587ca025b917774565521dde
Prosta-Olya <siverolya@gmail.com>
2025-06-03 22:40:10 +0500

    Add files via upload

---
+++ zd2/zd2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace zd2
+{
+    public partial class Form1 : Form
+    {
+        private Shop pyaterochka;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Product cola = new Product("Кола", 85);
+            //Product juice = new Product("Сок \"Добрый\"", 100);
+            //Console.WriteLine(cola.GetInfo());
+            //Console.WriteLine(juice.GetInfo());
+
+            pyaterochka = new Shop();
+            pyaterochka.CreateProduct("Кола", 85, 200);
+            pyaterochka.CreateProduct("Сок \"Добрый\"", 100, 50);
+            pyaterochka.WriteAllProducts();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] productsToSell = textBox1.Text.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries); // список товаров
+
+            pyaterochka.SellProducts(productsToSell.Select(p => p.Trim()).ToArray()); // продажа
+
+            pyaterochka.WriteAllProducts();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Текущая прибыль: {pyaterochka.Profit()} руб."); // вывод прибыли магазина
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Сумма чека вашей покупки: {pyaterochka.Check()} руб."); // вывод суммы чека покупки
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pyaterochka.NewSell();
+            MessageBox.Show("Вы можете начать новую покупку :)");
+        }
+    }
+}
---
+++ zd2/zd2/Shop.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zd2
+{
+    class Shop
+    {
+        // private List<Product> products;
+        private Dictionary<Product, int> products;
+        private decimal profit; // прибыль
+        private decimal check; // чек покупки
+        public Shop()
+        {
+            // products = new List<Product>();
+            products = new Dictionary<Product, int>();
+            profit = 0;
+        }
+        public void AddProduct(Product product, int count) // добавление продукта в список продуктов
+        {
+            products.Add(product, count);
+            //products.Add(product);
+        }
+        public void WriteAllProducts() // вывод всех продуктов
+        {
+            string listProducts = "Список всех продуктов:\n ";
+            foreach (var product in products)
+            {
+                //Console.WriteLine(product.GetInfo());
+                listProducts += product.Key.GetInfo() + "; Количество: " + product.Value + "\n";
+            }
+            MessageBox.Show(listProducts);
+        }
+        public void CreateProduct(string name, decimal price, int count) // метод для создания продукта и добавления его в список
+        {
+            //Product product = new Product(name, price);
+            //products.Add(product);
+            products.Add(new Product(name, price), count);
+        }
+        public void Sell(string ProductName) // покупка товара
+        {
+            Product ToSell = FindByName(ProductName);
+            if (ToSell != null)
+            {
+                foreach (var product in products)
+                {
+                    if (product.Key.Name == ProductName && product.Value > 0)
+                    {
+                        products[product.Key]--; // уменьшение количества продукта
+                        profit += product.Key.Price; // увеличение прибыли
+                        check += product.Key.Price; // увеличение суммы чека покупки
+                        MessageBox.Show($"Товар {ToSell.Name} продан успешно");
+                        return;
+                    }
+                }
+                MessageBox.Show("Товар закончился!");
+                //this.Sell(ToSell.Name);
+            }
+            else
+            {
+                MessageBox.Show("Товар не найден!");
+            }
+        }
+        public Product FindByName(string name) // поиск продукта по имени
+        {
+            foreach (var product in products.Keys)
+            {
+                if (product.Name == name)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+        public void SellProducts(string[] productNames) // продажа нескольких продуктов
+        { 
+            foreach (var name in productNames)
+            {
+                Sell(name);
+            }
+        }
+        public decimal Profit() // прибыль магазина
+        {
+            return profit;
+        }
+        public decimal Check()
+        {
+            return check;
+        }
+        public void NewSell()
+        {
+            check = 0;
+        }
+    }
+}
